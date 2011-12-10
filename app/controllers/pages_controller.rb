0404426e627ca645cf50fabb029110acbfcb6e57
Elvira Teran <xca367@my.utsa.edu>
2011-12-09 21:34:03 -0600

    done with static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,20 +1,26 @@
 class PagesController < ApplicationController
   def home
+    @title = "Home"
   end
 
   def profile
+    @title = "My Profile"
   end
 
   def contacts
+    @title = "Contacts"
   end
 
   def calendar
+    @title = "Calendar"
   end
 
   def discussions
+    @discussions = "Discussions"
   end
 
   def about
+    @about = "About"
   end
 
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,19 +1,30 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
     end
-  end
+    it "should have the right title" do
+          get 'home'
+          response.should have_selector("title",
+                            :content => "Gamma Phi Beta | Home")
+        end
+     end
 
   describe "GET 'profile'" do
     it "should be successful" do
       get 'profile'
       response.should be_success
     end
+    it "should have the right title" do
+          get 'profile'
+          response.should have_selector("title",
+                            :content => "Gamma Phi Beta | My Profile")
+        end
   end
 
   describe "GET 'contacts'" do
@@ -21,6 +32,11 @@ describe PagesController do
       get 'contacts'
       response.should be_success
     end
+    it "should have the right title" do
+          get 'contacts'
+          response.should have_selector("title",
+                            :content => "Gamma Phi Beta | Contacts")
+        end
   end
 
   describe "GET 'calendar'" do
@@ -28,6 +44,11 @@ describe PagesController do
       get 'calendar'
       response.should be_success
     end
+    it "should have the right title" do
+          get 'calendar'
+          response.should have_selector("title",
+                            :content => "Gamma Phi Beta | Calendar")
+        end
   end
 
   describe "GET 'discussions'" do
@@ -35,6 +56,11 @@ describe PagesController do
       get 'discussions'
       response.should be_success
     end
+    it "should have the right title" do
+          get 'discussions'
+          response.should have_selector("title",
+                            :content => "Gamma Phi Beta | Discussions")
+        end
   end
 
   describe "GET 'about'" do
@@ -42,6 +68,11 @@ describe PagesController do
       get 'about'
       response.should be_success
     end
+    it "should have the right title" do
+          get 'about'
+          response.should have_selector("title",
+                            :content => "Gamma Phi Beta | About")
+        end
   end
 
 end
